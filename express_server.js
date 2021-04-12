e6e2d44ee5424465700e4d566a0ad299e2fb1f40
JayShin <08jhs05@gmail.com>
2021-04-12 19:40:05 -0400

    update

--- express_server.js
+++ express_server.js
@@ -35,8 +35,9 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body.longURL;
-  res.redirect('/urls');
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect('/urls/' + newShortURL);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -50,7 +51,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL)
+  console.log(longURL);
   res.redirect(longURL);
 });
 
