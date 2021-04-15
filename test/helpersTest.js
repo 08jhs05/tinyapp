fc06678944811354f7cc618771b5e00f8ade51ad
JayShin <08jhs05@gmail.com>
2021-04-15 17:04:09 -0400

    add test file to test getUserByEmail function

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", users)
+    const expectedOutput = "userRandomID";
+
+    assert.equal(expectedOutput, user);
+  });
+
+  it('should return unidentified if email is not in database', function() {
+    const user = getUserByEmail("notInDatabase@example.com", users)
+    const expectedOutput = undefined;
+
+    assert.equal(expectedOutput, user);
+  });
+});
