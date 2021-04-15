06362354dda35b22eb7231e70ca5ba17f1a2c315
JayShin <08jhs05@gmail.com>
2021-04-15 16:23:22 -0400

    separate helper functions from express_server.js

---
+++ helpers.js
@@ -0,0 +1,40 @@
+// ===================================== helper functions =========================================
+
+function generateRandomString() {
+  let result           = [];
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+    result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+ }
+ return result.join('');
+}
+
+function getTemplateVars(urlDatabase, user, req){
+  return { urls: urlDatabase,
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] === undefined ? undefined : urlDatabase[req.params.shortURL].longURL,
+    user: user
+  }
+}
+
+function getUserByEmail(email, database){
+  for(user in database){
+    if(database[user].email === email) return user;
+  }
+  return false;
+}
+
+function urlsForUser(id, urlDatabase){
+  const result = {};
+  for(url in urlDatabase){
+    if(urlDatabase[url].userID === id) {
+      result[url] = {};
+      result[url].longURL = urlDatabase[url].longURL;
+      result[url].userID = urlDatabase[url].userID;
+    }
+  }
+  return result;
+}
+
+module.exports = { generateRandomString, getTemplateVars, getUserByEmail, urlsForUser };
